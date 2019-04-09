0ab00d6e9450a5b2a83cbb69122090641758260d
Duncan Brown <duncanjbrown@gmail.com>
2019-04-10 00:16:07 +0100

    Refactor step definitions
    Allow Stripe charges to be pushed into the fixture one by one, provide a
    means to modify them after they've gone, and return them using a method
    which wraps them in the right structure for ClearbooksCsv.

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,33 +1,44 @@
 require 'clearbooks_csv'
 
 module CSVGenerationHelper
-  def create_stripe_charges(hashes)
-    @stripe_charges = Struct.new(:data, :start_date, :end_date).new(
-      hashes.map do |h|
-        {
-          "balance_transaction": {
-            "amount": h[:amount], "fee": h[:fee], "currency": h[:currency]
-          },
-          "source": { "country": h[:country] }
-        }
-      end, Date.new(2019, 1, 1), Date.new(2019, 1, 31)
+  def get_stripe_charges
+    Struct.new(:data, :start_date, :end_date).new(
+      @stripe_charges, Date.new(2019, 1, 1), Date.new(2019, 1, 31)
     )
+  end
+
+  def add_stripe_charge(row)
+    @stripe_charges ||= []
+    @stripe_charges << row_to_stripe_hash(row)
+  end
+
+  def update_last_stripe_charge
+    @stripe_charges << yield(@stripe_charges.pop)
+  end
+
+  def row_to_stripe_hash(row)
+    {
+      "balance_transaction": {
+        "amount": row[:amount], "fee": row[:fee], "currency": row[:currency]
+      },
+      "source": { "country": row[:country] }
+    }
   end
 end
 
 World(CSVGenerationHelper)
 
 Given('there are the following charges in Stripe') do |table|
-  create_stripe_charges(table.hashes)
+  table.hashes.map { |row| add_stripe_charge(row) }
 end
 
 When('I run the CSV export for the period') do
-  @export = ClearbooksCsv.new(@stripe_charges)
+  @export = ClearbooksCsv.new(get_stripe_charges)
   @export.generate
 end
 
 Then('a CSV file should be created with the contents') do |table|
-  csv_contents = CSV.open(ClearbooksCsv.new(@stripe_charges).filename,
+  csv_contents = CSV.open(ClearbooksCsv.new(get_stripe_charges).filename,
                           headers: true).map(&:to_h)
 
   expect(table.hashes).to match(csv_contents)
